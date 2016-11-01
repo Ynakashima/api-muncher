fe299502ca73d228c2c7f5468ea4bcfe5e1415c9
Yuri Nakashima <yuri310@gmail.com>
2016-11-01 12:19:00 -0700

    added simplecov and test_helper gems, made edamam_api_wrapper file and related testing file, started self.list receipes method, url successfully returns search with app id and key in Postman

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,5 +1,20 @@
 require 'httparty'
 
 class EdamamApiWrapper
-  BASE_URL = "https://api.edamam.com/"
+  BASE_URL = "https://api.edamam.com/search"
+  ID = ENV["app_id"]
+  KEY = ENV["app_key"]
+
+  def self.listrecipes(search_term)
+    url = BASE_URL + "q=" + search_term + "&app_id=#{ID}&app_key=#{KEY}"
+    data = HTTParty.get(url)
+    recipe_list = []
+    if data["recipes"]
+      data["recipes"].each do |recipe|
+        wrapper = Recipe.new recipe["name"], recipe["id"] , purpose: recipe["purpose"], is_archived: recipe["is_archived"], members: recipe["members"]
+        recipe_list << wrapper
+      end
+    end
+    return recipe_list
+  end
 end
